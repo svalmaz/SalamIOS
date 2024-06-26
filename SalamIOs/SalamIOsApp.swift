bb74eee8ca7834dbc20a1561378fffea67b21727
Almaz <almaz.0.saparbaev@gmail.com>
2024-06-26 15:03:36 +0600

    Initial Commit

---
+++ SalamIOs/SalamIOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalamIOsApp.swift
+//  SalamIOs
+//
+//  Created by Almaz Saparbaev on 26/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SalamIOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
